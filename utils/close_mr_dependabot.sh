ded1d3868917d7480ab460dd75b6dfd0c84e13c0
Max Sharov <maxsharoff@gmail.com>
2019-11-02 13:47:05 +0300

    ci(scripts): fix getting MR number

--- utils/close_mr_dependabot.sh
+++ utils/close_mr_dependabot.sh
@@ -8,11 +8,13 @@ LISTMR=`curl --silent "${HOST}${CI_PROJECT_ID}/merge_requests?state=opened" --he
 COUNTBRANCHES=`echo ${LISTMR} | grep -o "\"source_branch\":\"${CI_COMMIT_REF_NAME}\"" | wc -l`;
 
 LIST_MR_GITHUB=`curl --silent "https://api.github.com/search/issues?q=${CI_COMMIT_SHA}" --header "Accept: application/vnd.github.v3+json"`;
-MR_NUMBER_GITHUB=`echo ${LIST_MR_GITHUB} | grep "\"number\": " | grep -o "[[:digit:]]"`;
+MR_NUMBER_GITHUB=`echo ${LIST_MR_GITHUB} | grep -o "\"number\": [[:digit:]]*" | grep -o "[[:digit:]]*"`;
 
 # Delete Github MR for opened gitlab MR
 if [ ${COUNTBRANCHES} -ne "0" ]; then
   if [ -n ${MR_NUMBER_GITHUB} ]; then
+    echo "Available github MR with number ${MR_NUMBER_GITHUB}"
+
     GITHUB_MR_BODY="{
         \"state\": \"closed\"
     }";
@@ -24,7 +26,6 @@ if [ ${COUNTBRANCHES} -ne "0" ]; then
         --data "${GITHUB_MR_BODY}";
     exit;
   fi
-
 fi
 
 echo "No availbale MR for closing";
