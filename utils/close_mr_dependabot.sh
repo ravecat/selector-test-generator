d761b57c98e45aa12115640e5802461ffdf1aeb0
Max Sharov <maxsharoff@gmail.com>
2019-11-04 02:11:18 +0300

    ci(scripts): fix search available github PR

--- utils/close_mr_dependabot.sh
+++ utils/close_mr_dependabot.sh
@@ -7,12 +7,16 @@
 LISTMR=`curl --silent "${HOST}${CI_PROJECT_ID}/merge_requests?state=opened" --header "PRIVATE-TOKEN:${PRIVATE_TOKEN}"`;
 COUNTBRANCHES=`echo ${LISTMR} | grep -o "\"source_branch\":\"${CI_COMMIT_REF_NAME}\"" | wc -l`;
 
-LIST_MR_GITHUB=`curl --silent "https://api.github.com/search/issues?q=head:${CI_COMMIT_REF_NAME}+repo:${CI_PROJECT_NAME}/${GITHUB_OWNER}" --header "Accept: application/vnd.github.v3+json"`;
-MR_NUMBER_GITHUB=`echo ${LIST_MR_GITHUB} | grep -o "\"number\": [[:digit:]]*" | grep -o "[[:digit:]]*"`;
-
 # Delete Github MR for opened gitlab MR
 if [ ${COUNTBRANCHES} -ne "0" ]; then
+  echo "Exist gitlab MR for current branch"
+
   if [ -n ${MR_NUMBER_GITHUB} ]; then
+    echo "Search github PR for ${CI_COMMIT_REF_NAME}"
+    
+    LIST_MR_GITHUB=`curl --silent "https://api.github.com/search/issues?q=head:${CI_COMMIT_REF_NAME}+repo:${GITHUB_OWNER}/${CI_PROJECT_NAME}" --header "Accept: application/vnd.github.v3+json"`;
+    MR_NUMBER_GITHUB=`echo ${LIST_MR_GITHUB} | grep -o "\"number\": [[:digit:]]*" | grep -o "[[:digit:]]*"`;
+    
     echo "Available github MR with number ${MR_NUMBER_GITHUB}"
 
     GITHUB_MR_BODY="{
