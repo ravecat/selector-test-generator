2c3e0dab7084a616add082aa6f1a90cb9e6006e0
Max Sharov <maxsharoff@gmail.com>
2019-11-04 22:51:13 +0300

    ci(scripts): update search github PR

--- utils/close_mr_dependabot.sh
+++ utils/close_mr_dependabot.sh
@@ -14,20 +14,20 @@ if [ ${COUNTBRANCHES} -ne "0" ]; then
   if [ -n ${MR_NUMBER_GITHUB} ]; then
     echo "Search github PR for ${CI_COMMIT_REF_NAME}"
     
-    LIST_MR_GITHUB=`curl --silent "https://api.github.com/search/issues?q=head:${CI_COMMIT_REF_NAME}+repo:${GITHUB_OWNER}/${CI_PROJECT_NAME}" --header "Accept: application/vnd.github.v3+json"`;
+    LIST_MR_GITHUB=`curl --silent "https://api.github.com/search/issues?q=state:open+head:${CI_COMMIT_REF_NAME}+repo:${GITHUB_OWNER}/${CI_PROJECT_NAME}" --header "Accept: application/vnd.github.v3+json"`;
     MR_NUMBER_GITHUB=`echo ${LIST_MR_GITHUB} | grep -o "\"number\": [[:digit:]]*" | grep -o "[[:digit:]]*"`;
     
     echo "Available github MR with number ${MR_NUMBER_GITHUB}"
 
-    GITHUB_MR_BODY="{
-        \"state\": \"closed\"
-    }";
+    # GITHUB_MR_BODY="{
+    #     \"state\": \"closed\"
+    # }";
 
-    curl -X PATCH "https://api.github.com/repos/${GITHUB_OWNER}/${CI_PROJECT_NAME}/pulls/${MR_NUMBER_GITHUB}" \
-        --header "Accept: application/vnd.github.v3+json" \
-        --header "Authorization: token ${GITHUB_TOKEN}" \
-        --header "Content-Type: application/json" \
-        --data "${GITHUB_MR_BODY}";
+    # curl -X PATCH "https://api.github.com/repos/${GITHUB_OWNER}/${CI_PROJECT_NAME}/pulls/${MR_NUMBER_GITHUB}" \
+    #     --header "Accept: application/vnd.github.v3+json" \
+    #     --header "Authorization: token ${GITHUB_TOKEN}" \
+    #     --header "Content-Type: application/json" \
+    #     --data "${GITHUB_MR_BODY}";
     exit;
   fi
 fi
